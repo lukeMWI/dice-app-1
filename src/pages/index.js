f7c26bca96a2c25c532af6b39f69ecf9d3fdddca
Luke <mwishare@MWIs-MacBook-Pro.local>
2020-08-14 11:13:36 -0500

    [feat] Added new favicon and cursor. Added Page Wrap. Renamed components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from '@material-ui/core/Container'
 import Header from "../components/Header";
-import Die from "../components/Die-fc";
+import Die from "../components/Die";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/global.css";
 
 export default function Home() {
   return (
-  	<Container>
-	  	<Header />
-  		<div className="dice-flex">
-			  <Die sides={ 6 } />
-			  <Die sides={ 8 } />
-			  <Die sides={ 12 } />
-			  <Die sides={ 20 } />
-		  </div>
-	  </Container>
+  	<div class="page-wrap">
+	  	<Container>
+		  	<Header />
+	  		<div className="dice-flex">
+				  <Die sides={ 6 } />
+				  <Die sides={ 8 } />
+				  <Die sides={ 12 } />
+				  <Die sides={ 20 } />
+			  </div>
+		  </Container>
+	  </div>
   );
 }
